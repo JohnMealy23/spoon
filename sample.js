aacdfa9db899da6d6a0fb52b98d8c8536a05f888
mikefagerness <58198252+mikefagerness@users.noreply.github.com>
2019-12-26 09:46:51 -0800

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,1 +1,26 @@
-console.log('something')
+//probability homework
+
+const adjustableTrue = (probability) => {
+        if (const results < 30) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+console.log(probability)
+
+// What follows is a unit test.  Don't touch anything below this line!
+// It'll run your function 100 times, and you can check if the number of
+// trues is as intended - around ~30% of the whole, in this case.
+const percentTrue = 30
+const results = { trues: 0, falses: 0 }
+for (let i = 0; i < 100; i++) {
+    const result = adjustableTrue(percentTrue)
+    if (result === true) {
+        results.trues++
+    } else if (result === false) {
+        results.falses++
+    }
+}
+console.log({ results })
