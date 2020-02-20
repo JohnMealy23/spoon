bc1d78c5b74c379ca3d370d2c0f97992002b7124
Mike Fagerness <mike.fagerness@gmail.com>
2020-02-19 18:51:45 -0800

    I did it

---
+++ src/john/index.js
@@ -0,0 +1,33 @@
+const jokes = require('give-me-a-joke')
+
+
+const callback = (joke) => {
+    console.log(joke)
+}
+// jokes.getRandomDadJoke(callback)
+jokes.getCustomJoke('Mike', 'Fagerness', callback)
+
+
+
+
+
+
+// const myCallback = () => {
+//     const myLabel = document.createElement('div')
+//     myLabel.style.background = 'red'
+//     myLabel.innerText = "Hello Friends!"
+//     document.body.appendChild(myLabel)
+// }
+
+// const button = document.createElement('button')
+// button.innerText = 'Add More Friends!'
+// button.addEventListener('click', myCallback)
+// document.body.appendChild(button)
+
+
+// const input = document.createElement('input')
+// document.body.appendChild(input)
+
+// button.addEventListener('click', () => {
+//     alert(input.value)
+// })
--- src/mike/sample.js
+++ src/mike/sample.js
@@ -11,6 +11,7 @@
 //
 //array1.forEach(makeHello)
 
+/*
 const countDown = (remaining) => {
     console.log(`${remaining} left to go!`)
     if (remaining !== 0) {
@@ -31,6 +32,7 @@ any number greater than 0 will have 1 subtracted from it
 //equals Math.random function
 //equals 50%
 
+/*
 const getRandomBoolean = (probability) => {
     const randomNumber = Math.random()
     const spoonCat = randomNumber >= probability;
@@ -107,3 +109,168 @@ for (let i = 0; i < 100; i++) {
     }
 }
 console.log({ results })
+*/
+
+/*
+const arr = ['back', 'oh', 'yah']
+const callback = (acc, item) => {
+    const sum = acc + ' ' + item
+    return sum
+}
+const result = arr.reduce(callback, 'backstreet')
+console.log(result)
+*/
+/*
+const foodArray = [
+    {
+        food: 'hamburger',
+        acceptableCondiments: [
+            'mayo',
+            'mustard',
+            'relish',
+            'cheese',
+        ],
+    },
+    {
+        food: 'baked potato',
+        acceptableCondiments: [
+            'butter',
+            'sour cream',
+            'cheese',
+            'chives',
+        ],
+    },
+    {
+        food: 'tacos',
+        acceptableCondiments: [
+            'salsa',
+            'sour cream',
+            'cheese',
+            'gauc',
+        ],
+    },
+    {
+        food: 'condiment soup',
+        acceptableCondiments: [
+            'mayo',
+            'mustard',
+            'relish',
+            'butter',
+            'chives',
+            'salsa',
+            'sour cream',
+            'cheese',
+            'gauc',
+        ],
+    },
+]
+
+//The foodNames parameter should be an array of all of the 
+// food names in foodArray.
+//The allCondiments parameter should be an array of all 
+// of the condiments in foodArray, with redundancies.
+
+/*
+const callback = (acc, item) => {
+    const foodName = item.food
+    acc.push(foodName)
+    return acc
+}
+
+const array2 = []
+const result = foodArray.reduce(callback, array2)
+console.log(result)
+
+const callback2 = (acc, item) => {
+    const condimentName = item.acceptableCondiments
+    acc.push(condimentName)
+    return acc
+}
+
+const array3 = []
+const result2 = foodArray.reduce(callback2, array3)
+const result3 = result2.flat()
+console.log(JSON.stringify(result3, null, 4))
+*/
+
+const foodArray = [
+    {
+        food: 'hamburger',
+        acceptableCondiments: [
+            'mayo',
+            'mustard',
+            'relish',
+            'cheese',
+        ],
+    },
+    {
+        food: 'baked potato',
+        acceptableCondiments: [
+            'butter',
+            'sour cream',
+            'cheese',
+            'chives',
+        ],
+    },
+    {
+        food: 'tacos',
+        acceptableCondiments: [
+            'salsa',
+            'sour cream',
+            'cheese',
+            'gauc',
+        ],
+    },
+    {
+        food: 'condiment soup',
+        acceptableCondiments: [
+            'mayo',
+            'mustard',
+            'relish',
+            'butter',
+            'chives',
+            'salsa',
+            'sour cream',
+            'cheese',
+            'gauc',
+        ],
+    },
+]
+/*
+Remember how we learned to make DOM elements with things like document.createElement, 
+element.innerText, and document.body.appendChild?  Let's do that again.  But this time, 
+we're going to user foodArray.
+* Loop through foodArray.
+* For every food parameter, insert the value inside of a h2 tag.
+* Under each h2 tag, create an unorganized list, and insert each acceptableCondiments item in that unorganized list as a list item.
+*/
+
+const justTheFood = foodArray.map((menuItem) => {
+    const foodName = menuItem.food
+    const h2a = document.createElement("h2"); 
+    h2a.innerText = foodName
+    document.body.appendChild(h2a)
+
+    const condimentNames = menuItem.acceptableCondiments
+    const ul = document.createElement("ul");
+    document.body.appendChild(ul)
+    condimentNames.forEach(condiment => {
+        const li = document.createElement("li");
+        li.innerText = condiment
+        ul.appendChild(li)
+    });
+});
+
+const h1 = document.createElement("h1"); 
+const t1 = document.createTextNode(justTheFood); 
+
+h.appendChild(t1);  
+document.body.appendChild(h1);
+
+const justTheCondiments = foodArray.map(menuItem => menuItem.acceptableCondiments);
+
+const h2 = document.createElement("h2"); 
+const t2 = document.createTextNode(justTheCondiments); 
+
+h.appendChild(t2); 
+document.body.appendChild(h2);
